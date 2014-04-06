a09bb8012e1dbeb5eccb6b3c04f69a1545e2ef0d
Wanderson Braganca <wanderson.wbc@gmail.com>
2014-04-06 19:08:03 -0300

    Reformat code te be PSR-2 compatible

--- CpfValidator.php
+++ CpfValidator.php
@@ -1,74 +1,61 @@
 <?php
-
 /**
- * @link https://github.com/leandrogehlen/yii2-br-validator
- * @license https://github.com/leandrogehlen/yii2-br-validator/blob/master/LICENSE
+ * @link https://github.com/yiibr/yii2-br-validator
+ * @license https://github.com/yiibr/yii2-br-validator/blob/master/LICENSE
  */
 namespace yiibr\brvalidator;
-
 
 use yii\validators\Validator;
 use Yii;
 
 /**
- * CpfValidator checks if the attribute value is a valid CPF. 
+ * CpfValidator checks if the attribute value is a valid CPF.
  *
  * @author Leandro Gehlen <leandrogehlen@gmail.com>
- * @since 2.0
  */
+class CpfValidator extends Validator
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->message === null) {
+            $this->message = Yii::t('yii', "{attribute} is invalid.");
+        }
+    }
 
-class CpfValidator extends Validator {
-	
-	/**
-	 * @inheritdoc
-	 */
-	public function init()
-	{
-		parent::init();
-		if ($this->message === null) {
-			$this->message = Yii::t('yii', "{attribute} is invalid.");
-		}
-	}
-		
-	/**
-	 * @inheritdoc
-	 */
-	protected function validateValue($value) {
-		
-		$valid = true;
-		$cpf = str_pad(preg_replace('/[^0-9_]/', '', $value ), 11, '0', STR_PAD_LEFT);
-		
-		
-		for($x = 0; $x < 10; $x ++) {
-			if ($cpf == str_repeat ( $x, 11 )) {
-				$valid = false;
-			}
-		}
-					
-		if ($valid) {
-			if (strlen ( $cpf ) != 11) {
-				$valid = false;
-								
-			} else {
-				for($t = 9; $t < 11; $t ++) {
-					$d = 0;
-					
-					for($c = 0; $c < $t; $c ++) {
-						$d += $cpf {$c} * (($t + 1) - $c);
-					}
-					
-					$d = ((10 * $d) % 11) % 10;
-					
-					if ($cpf{$c} != $d) {
-						$valid = false;
-						break;
-					}
-				}				
-			}
-		}		
-		
-		return ($valid) ? [] : [$this->message, []];		
-	}
+    /**
+     * @inheritdoc
+     */
+    protected function validateValue($value)
+    {
+        $valid = true;
+        $cpf = str_pad(preg_replace('/[^0-9_]/', '', $value ), 11, '0', STR_PAD_LEFT);
+
+        for($x = 0; $x < 10; $x ++) {
+            if ($cpf == str_repeat ( $x, 11 )) {
+                $valid = false;
+            }
+        }
+        if ($valid) {
+            if (strlen ( $cpf ) != 11) {
+                $valid = false;
+            } else {
+                for ($t = 9; $t < 11; $t ++) {
+                    $d = 0;
+                    for($c = 0; $c < $t; $c ++) {
+                        $d += $cpf {$c} * (($t + 1) - $c);
+                    }
+                    $d = ((10 * $d) % 11) % 10;
+                    if ($cpf{$c} != $d) {
+                        $valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return ($valid) ? [] : [$this->message, []];
+    }
 }
-
-?>
--- tests/unit/CnpjValidatorTest.php
+++ tests/unit/CnpjValidatorTest.php
@@ -11,22 +11,19 @@ use yiibr\brvalidator\CnpjValidator;
  * CpfValidatorTest
  */
 class CnpjValidatorTest extends TestCase
-{				
+{
+    public function testValidateValue()
+    {
+        $val = new CnpjValidator();
+        $this->assertFalse($val->validate('789542284'));
 
-	public function testValidateValue()
-	{
-		$val = new CnpjValidator();
-		$this->assertFalse($val->validate('789542284'));
-				
-		$this->assertFalse($val->validate('22222222222222'));
-		$this->assertFalse($val->validate('22.222.222/2222-22'));
-		
-		$this->assertFalse($val->validate('32.458.657.0001-89'));
-		$this->assertFalse($val->validate('32458657000189'));
-		
-		$this->assertTrue($val->validate('62.346.464/0001-01'));
-		$this->assertTrue($val->validate('62346464000101'));		
-	}	
+        $this->assertFalse($val->validate('22222222222222'));
+        $this->assertFalse($val->validate('22.222.222/2222-22'));
+
+        $this->assertFalse($val->validate('32.458.657.0001-89'));
+        $this->assertFalse($val->validate('32458657000189'));
+
+        $this->assertTrue($val->validate('62.346.464/0001-01'));
+        $this->assertTrue($val->validate('62346464000101'));
+    }
 }
-
-?>
