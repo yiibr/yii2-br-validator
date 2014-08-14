d43d21d57b1522d4335790591692cb7aaf7dacc7
GuilhermeLessa <gl-lessa@hotmail.com>
2014-08-14 09:49:07 -0300

    Update yiibr.validation.js

--- assets/yiibr.validation.js
+++ assets/yiibr.validation.js
@@ -66,6 +66,76 @@ yiibr.validation = (function($) {
                 pub.addMessage(messages, options.message, value);
             }
         },
+        cnpj: function(value, messages, options) {
+            if (options.skipOnEmpty && pub.isEmpty(value)) {
+                return;
+            }
+
+            String.prototype.repeat = function(num) {
+                return new Array(isNaN(num) ? 1 : ++num).join(this);
+            }
+
+            var valid = true;
+
+            cnpj = value.replace(/[^\d]+/g, '');
+
+            if (cnpj == "00000000000000" ||
+                    cnpj == "11111111111111" ||
+                    cnpj == "22222222222222" ||
+                    cnpj == "33333333333333" ||
+                    cnpj == "44444444444444" ||
+                    cnpj == "55555555555555" ||
+                    cnpj == "66666666666666" ||
+                    cnpj == "77777777777777" ||
+                    cnpj == "88888888888888" ||
+                    cnpj == "99999999999999") {
+                valid = false;
+            }
+
+            if (cnpj == '') {
+                valid = false;
+            }
+
+            if (cnpj.length != 14) {
+                valid = false;
+            }
+
+            tamanho = cnpj.length - 2;
+            numeros = cnpj.substring(0, tamanho);
+            digitos = cnpj.substring(tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2) {
+                    pos = 9;
+                }
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado != digitos.charAt(0)) {
+                valid = false;
+            }
+
+            tamanho = tamanho + 1;
+            numeros = cnpj.substring(0, tamanho);
+            soma = 0;
+            pos = tamanho - 7;
+            for (i = tamanho; i >= 1; i--) {
+                soma += numeros.charAt(tamanho - i) * pos--;
+                if (pos < 2) {
+                    pos = 9;
+                }
+            }
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+
+            if (resultado != digitos.charAt(1)) {
+                valid = false;
+            }
+
+            if (!valid) {
+                pub.addMessage(messages, options.message, value);
+            }
+        }
     };
     return pub;
 })(jQuery);
