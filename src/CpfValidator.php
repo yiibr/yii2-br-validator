839c2ca35ef6d5c17f67ec68abf55a9beb525e4a
Almir Bolduan <almir.bsi@gmail.coim>
2020-06-03 12:50:04 -0300

    Atualização compatibilidade PHP 7.4. Corrige o erro: "Array and string offset access syntax with curly braces is deprecated".

--- src/CpfValidator.php
+++ src/CpfValidator.php
@@ -47,10 +47,10 @@ class CpfValidator extends DocumentValidator
                 for ($t = 9; $t < 11; $t ++) {
                     $d = 0;
                     for($c = 0; $c < $t; $c ++) {
-                        $d += $cpf {$c} * (($t + 1) - $c);
+                        $d += $cpf[$c] * (($t + 1) - $c);
                     }
                     $d = ((10 * $d) % 11) % 10;
-                    if ($cpf{$c} != $d) {
+                    if ($cpf[$c] != $d) {
                         $valid = false;
                         break;
                     }
